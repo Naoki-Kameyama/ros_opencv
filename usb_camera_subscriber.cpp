6d4dc8c483811678a2d225cfac7b4e201f3fb5d2
Shohei-Nogami <14ej081@ms.dendai.ac.jp>
2017-04-25 03:41:42 +0900

    Update usb_camera_subscriber.cpp

--- usb_camera_subscriber.cpp
+++ usb_camera_subscriber.cpp
@@ -8,16 +8,10 @@
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
 	try{
-		ROS_INFO_STREAM("Starrrrt_func");
-        cv_bridge::CvImagePtr cv_ptr;
-		cv_ptr=cv_bridge::toCvCopy(msg, (sensor_msgs::image_encodings::BGR8));
-//		cv::imshow("Image_Subscriber",cv_bridge::toCvShare(msg,
-//					"rgb8")->image);
-		cv::imshow("Image_Subscriber",cv_ptr->image);
-//	    cv::imwrite("Teste.png", cv_ptr->image);
+	        cv_bridge::CvImagePtr cv_ptr;
+		cv::imshow("Image_Subscriber",cv_bridge::toCvShare(msg,
+					"rgb8")->image);
 		
-		ROS_INFO_STREAM("Starrrrt");
-
 		if(cv::waitKey(30)>=0)
 			ros::shutdown();
 		}
@@ -29,28 +23,27 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 int main(int argc,char **argv)
 {
-	ROS_INFO_STREAM("Starrrrt");
+
 	ros::init(argc,argv,"camera_subscriber");
-	ROS_INFO_STREAM("Starrrrt");
+
 
 	ros::NodeHandle nh;
 	cv::namedWindow("Image_Subscriber");
 	cv::startWindowThread();
 
-    ROS_INFO_STREAM("Starrrrt");
+
 
 	image_transport::ImageTransport it(nh);	
 	image_transport::Subscriber sub;
 
-	ROS_INFO_STREAM("Starrrrt_sub_prev");
 
 	sub = it.subscribe("/usb_cam/image_raw",1,imageCallback);
 
-	ROS_INFO_STREAM("Starrrrt");
+
 
 	ros::spin();
 	cv::destroyWindow("camera_subscriber_sub_after");
-	ROS_INFO_STREAM("Errror");
+
 	return EXIT_SUCCESS;
 }
 
